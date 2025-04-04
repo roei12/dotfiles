1fc35e9b7a0e8448b08086e3cb342b0b295dcfed
roei levi <roei1234567@gmail.com>
2025-04-04 20:37:24 +0300

    added basic lsp keybinds

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,13 @@ return {
     'nvim-lspconfig',
     event = 'BufReadPost',
     after = function()
+        local on_attach = function(client, bufnr)
+            local bufopts = { noremap=true, silent=true, buffer=bufnr }
+            vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
+            vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
+            vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, bufopts)
+        end
         local capabilities = require(vim.g.cmp_plugin).get_lsp_capabilities()
-        require'lspconfig'.clangd.setup{ capabilities = capabilities }
+        require'lspconfig'.clangd.setup{ on_attach=on_attach, capabilities = capabilities }
     end
 }
