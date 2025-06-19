8a7ff3c5fcec3933239e14633ac16c1aed1d011b
roei levi <roei1234567@gmail.com>
2025-06-20 00:11:34 +0300

    updated plugin versions and lspconfig configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,14 +1,25 @@
 return {
-    'nvim-lspconfig',
-    event = 'BufReadPost',
-    after = function()
-        local on_attach = function(client, bufnr)
-            local bufopts = { noremap=true, silent=true, buffer=bufnr }
-            vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
-            vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
-            vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, bufopts)
+    {
+        'nvim-lspconfig',
+        event = 'BufReadPost',
+        lazy = false,
+        after = function()
+            local on_attach = function(client, bufnr)
+                local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, bufopts)
+                vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, bufopts)
+                vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format, bufopts)
+                vim.keymap.set('n', 'gl', vim.diagnostic.open_float, bufopts)
+            end
+            local capabilities = require(vim.g.cmp_plugin).get_lsp_capabilities()
+            local lsp = vim.lsp
+            lsp.config('*', {
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+            lsp.enable('clangd')
+            lsp.enable('gleam')
+            lsp.enable('lua_ls')
         end
-        local capabilities = require(vim.g.cmp_plugin).get_lsp_capabilities()
-        require'lspconfig'.clangd.setup{ on_attach=on_attach, capabilities = capabilities }
-    end
+    },
 }
