ee5459e820f649548293f5c6040d44e53d15895c
Roei Levi <roei1234567@gmail.com>
2025-08-15 12:10:21 +0300

    added telescope bind for references

--- .config/nvim/lua/plugins/picker.lua
+++ .config/nvim/lua/plugins/picker.lua
@@ -3,6 +3,7 @@ return {
     keys = {
         {'<leader>ff', "<CMD>Telescope find_files<CR>"},
         {'<leader>fg', "<CMD>Telescope live_grep<CR>"},
+        {'<leader>fr', "<CMD>Telescope lsp_references<CR>"},
     },
     after = function()
         require'telescope'.setup{}
