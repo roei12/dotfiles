5e65946f5aff1f6bc7b996ef0f5c951eb0385903
Roei Levi <roei@brava.security>
2026-01-09 17:22:05 +0200

    added pyright and ruff lsp configuration and updated colorscheme

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -7,7 +7,27 @@ return {
             vim.lsp.enable('gleam')
             vim.lsp.enable('lua_ls')
             vim.lsp.enable('gopls')
+            vim.lsp.config('pyright', {
+                settings = {
+                    python = {
+                        analysis = {
+                            typeCheckingMode = "Strict",
+                        },
+                    },
+                },
+                on_attach = function(client, _)
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end,
+            })
             vim.lsp.enable('pyright')
+            vim.lsp.config('ruff', {
+                on_attach = function(client, _)
+                    client.server_capabilities.documentFormattingProvider = true
+                end
+
+            })
+            vim.lsp.enable('ruff')
             vim.lsp.config('rust_analyzer', {
                 settings = {
                     ['rust-analyzer'] = {
--- .config/nvim/lua/plugins/visuals.lua
+++ .config/nvim/lua/plugins/visuals.lua
@@ -1,19 +1,10 @@
 return {
-    -- {
-    --     'gruvbox-material',
-    --     lazy = false,
-    --     after = function ()
-    --         vim.cmd.colorscheme('gruvbox-material')
-    --     end
-    -- },
     {
-        'vim-moonfly-colors',
+        'darkbox.nvim',
         lazy = false,
         after = function()
-            require('moonfly').custom_colors({
-                bg = "#000000",
-            })
-            vim.cmd.colorscheme('moonfly')
+            require('darkbox').load()
+            vim.cmd.colorscheme('darkbox')
         end
     },
     {
@@ -29,9 +20,9 @@ return {
         lazy = true,
         event = 'BufReadPost',
         after = function()
-            require'treesitter-context'.setup {
+            require 'treesitter-context'.setup {
                 enable = true,
             }
         end,
-    }
+    },
 }
