0a4e02b768e451bb40560988f8ebf677555fec19
Roei Levi <roei@brava.security>
2026-01-16 22:46:55 +0200

    removed wezterm window decorations

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,8 @@ config.keys = require('keybinds')
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.font_size = 14
 
+config.window_decorations = "NONE"
+
 config.leader = { key = ' ', mods = 'ALT', timeout_milliseconds = 1000 }
 
 return config
